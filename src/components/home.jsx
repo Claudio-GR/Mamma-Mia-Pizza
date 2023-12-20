3a09f1bcff5d19d2ec55a7fd01f60f76192033be
Claudio-GR <claudio.gonzalez.rivera@gmail.com>
2023-12-19 22:28:25 -0300

    adding and sustracting pizzas

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,17 +2,63 @@ import { useContext } from "react";
 import { Pizzas_context } from "../context/pizza-menu";
 
 const Home = () => {
-    const {Pizzas, setPizzas} = useContext(Pizzas_context)
-    console.log(Pizzas)
+    const {Pizzas, SetPizzas, Total_cart, SetTotal_cart} = useContext(Pizzas_context)
+
+    const Total_Calculator= () =>{
+        const Cart_Calculator = Pizzas.map(pizza => pizza.Total); 
+       console.log('Cart Calculator: ', Cart_Calculator)
+   
+       SetTotal_cart(Cart_Calculator.reduce((a, b) => a+b, 0))    
+       console.log('Total Cart: ', Total_cart)
+    }
+    
+    const Add_qty = (id) => {
+        console.log('Pizzas befor change', Pizzas)    
+        const newQtys = Pizzas.map((pizza)=> {
+            if(pizza.id===id) {
+              return {
+                ...pizza, Qty: pizza.Qty+1, Total: (pizza.Qty+1)*pizza.price
+              }
+            }
+            return pizza
+            }
+          );
+          SetPizzas(newQtys);
+          console.log('Pizzas after change', Pizzas)    
+          Total_Calculator();
+    }
+    const Sus_qty = (id) => {
+        console.log('Pizzas befor change', Pizzas)    
+        const newQtys = Pizzas.map((pizza)=> {
+            if(pizza.id===id && pizza.Qty!==0) {
+              return {
+                ...pizza, Qty: pizza.Qty-1, Total: (pizza.Qty-1)*pizza.price
+              }
+            }
+            return pizza
+            }
+          );
+          SetPizzas(newQtys);
+          console.log('Pizzas after change', Pizzas)    
+          Total_Calculator();
+    }
     return (
         <div className="App">
             <h1>Pizza Mamma Mia!</h1>
+            <h2>{Total_cart}</h2>
             <div className="gallery grid-columns-5 p-3">
                 {Pizzas.map((pizza) => (
-                    <div /*onClick={() => addlike(picture.id)} className="photo" style={{backgroundImage: `url(${picture.src.medium})`}}*/ key={pizza.id}>
+                    <div key={pizza.id}>
                         <section>
+                            <img src={pizza.img} alt="" />
                             <h3>{pizza.name}</h3>
-                            <p> {pizza.desc} </p> {/* aquí se inserta la descripción de la fotografía */}
+                            <p> {pizza.price} </p> {/* aquí se inserta la descripción de la fotografía */}
+                            <div>
+                                <button onClick={() => Sus_qty(pizza.id)}>-</button>
+                                <p>{pizza.Qty}</p>
+                                <button onClick={() => Add_qty(pizza.id)}>+</button>
+                                <p>{pizza.Total}</p>
+                            </div>
                             {/* aquí se podría agregar más info sobre la foto */}
                         </section>
                     </div>
--- src/context/pizza-menu.jsx
+++ src/context/pizza-menu.jsx
@@ -5,6 +5,8 @@ export const Pizzas_context = createContext();
 
 const Pizzas_provider = ({ children}) => {
     const [Pizzas, SetPizzas] = useState([])
+    const [Total_cart, SetTotal_cart] = useState(0)
+
 
     const Pizzas_data = async () => {
         try {
@@ -13,23 +15,24 @@ const Pizzas_provider = ({ children}) => {
              if(!resolution.ok){
                  throw new Error ("Hay un error en la data");
              }
-             console.log("respuesta de la api", resolution)
+             //console.log("respuesta de la api", resolution)
             
              const Pizza_data =await resolution.json();
-             console.log('this is picture_data', Pizza_data);
+             //console.log('this is picture_data', Pizza_data);
              SetPizzas(Pizza_data.map((pizza) => ({...pizza, Qty:0, Total:0})))
-             console.log('context data: ', Pizzas)
+             //console.log('context data: ', Pizzas)
          } catch(error){
              console.error({message:error})
          }
      }
+     
 
     useEffect(() =>{
         Pizzas_data();
     }, []);
 
     return (
-        <Pizzas_context.Provider value={{Pizzas, SetPizzas}}>
+        <Pizzas_context.Provider value={{Pizzas, SetPizzas, Total_cart, SetTotal_cart}}>
             {children}
         </Pizzas_context.Provider>)
 }
